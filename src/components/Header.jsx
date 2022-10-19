402d2a5d1fb94e39a4923ae7a859c8a28c60aafa
X-CYL <castellastro@gmail.com>
2022-10-19 19:29:19 +0200

    create sass working directories,footer component and starting header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div className='container'>
+            <div className='header'>
+                <p> header a fabriquer</p>
+            </div>
+        </div>
+        
+    )
+}
