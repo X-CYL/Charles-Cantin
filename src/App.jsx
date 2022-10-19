2473687dc8f538edbdcd1c7bad1b6164946f27dd
X-CYL <castellastro@gmail.com>
2022-10-19 22:01:24 +0200

    create header, and import instagram and facebook logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './components/Footer';
-import Header from './components/Header'
+import Header from './components/Header';
+
 function App() {
   return (
     <div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Col, Row } from 'antd'
+import logofacebook from '../assets/pics/facebook-logo.png'
+import logoinstagram from '../assets/pics/instagram-logo.png'
 
 export default function Header() {
     return (
         <div className='container'>
             <div className='header'>
-                <p> header a fabriquer</p>
-            </div>
+                <Row>
+                    <Col xs={6} sm={6} md={8} lg={10} />
+                        <img className='logo-size' src= { logofacebook } alt="logo de facebook" />
+                        <img className='logo-size' src= { logoinstagram } alt="logo de instagram" />
+                    <Col xs={6} sm={6} md={8} lg={10} />
+                </Row>
+            </div>    
+            
         </div>
         
     )
+
 }
