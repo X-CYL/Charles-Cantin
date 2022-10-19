9ae4e3c449d86e8db3fd35d142ff282d8936bd74
X-CYL <castellastro@gmail.com>
2022-10-19 23:10:12 +0200

    add more picts to build the site and continuing building the index page

---
+++ src/components/IndexContent.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import bgpics from '../assets/pics/charte/snow2.jpg'
+
+export default function IndexContent() {
+    return (
+        <div className='indexContaint'>
+           <img className='imgBg' src= { bgpics } alt="paysage panoramique de montagne en fond " />
+        </div>
+    );
+};
